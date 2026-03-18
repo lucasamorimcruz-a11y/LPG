721cce6673a17e379bed95fa2d793a1cc693b829
lucas <lucas.amorim.cruz@gmail.com>
2026-03-18 03:01:38 -0300

    update

---
+++ lista1/5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%i", &n);
+    while (n--)
+    {
+        double a, b, c;
+        scanf("%lf %lf %lf", &a, &b, &c);
+        printf("A média desses valores são: %.1lf\n", ((a * 2) + (b * 3) + (c * 5)) / 10.0);
+    }
+}
